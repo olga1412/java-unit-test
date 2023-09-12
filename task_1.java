fbd53c795f822b3fad0fdf27d0426f5dd3e3916f
Olga Yasinskaya <olga.y1412@bk.ru>
2023-09-12 12:20:21 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,8 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+    boolean expected = true;
+    
+    assertEquals("Должно вернуться true, так как пользователю больше 18 лет", expected, isAdult);
+}
